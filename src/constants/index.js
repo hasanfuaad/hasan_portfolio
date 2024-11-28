e84e6dff45e908f8bfd3ac75e3f2c3d52222140e
hasanfuaad02 <hasan.fuaad02@gmail.com>
2024-11-28 19:07:24 +0300

    added testimonials

--- src/constants/index.js
+++ src/constants/index.js
@@ -162,28 +162,13 @@ const experiences = [
 const testimonials = [
   {
     testimonial:
-      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
-    name: "Sara Lee",
-    designation: "CFO",
-    company: "Acme Co",
-    image: "https://randomuser.me/api/portraits/women/4.jpg",
+      "Hasan is a passionate, experienced, and highly skilled professional. I’ve had the pleasure of working with him, and his expertise consistently impressed me. His problem-solving abilities and dedication reflect a strong commitment to personal growth. I thoroughly enjoyed collaborating with Hasan and highly recommend him for any project or team.",
+    name: "Ahmad Alrai",
+    designation: "full-stack developer",
+    company: "RTS",
+    image: "https://ahmadalrai.com/_next/image?url=%2FintroImg.jpg&w=256&q=95",
   },
-  {
-    testimonial:
-      "I've never met a web developer who truly cares about their clients' success like Rick does.",
-    name: "Chris Brown",
-    designation: "COO",
-    company: "DEF Corp",
-    image: "https://randomuser.me/api/portraits/men/5.jpg",
-  },
-  {
-    testimonial:
-      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
-    name: "Lisa Wang",
-    designation: "CTO",
-    company: "456 Enterprises",
-    image: "https://randomuser.me/api/portraits/women/6.jpg",
-  },
+  
 ];
 
 const projects = [
